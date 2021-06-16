6faf024e1547b5a2a77bb4b0ba7d2914f1ba2a3d
Lili Cohen <lili.cohen.tv@gmail.com>
2021-06-15 17:24:59 -0700

    changing app

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <Nav />
+
+          <Route exact path={['/', '/home']} component={Home} />
+          <Route exact path="/bandprofile/" component={BandProfile} />
+          <Route path="/profile/:id" component={BandProfile} />
+          <Footer />
+          {/* <LeafMap /> */}
+        </div>
+      </Router>
+      <h1>Lyn's Plants!</h1>
     </div>
   );
 }
