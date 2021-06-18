d5ca1f6a20b5d04910dfb67549ea0c8c183eed0d
Lili Cohen <lili.cohen.tv@gmail.com>
2021-06-17 20:23:55 -0700

    adding materialize

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -4,7 +4,7 @@ import CatImg from '../../images/cat-plant-home.png';
 
 export default function Home() {
   return (
-    <div>
+    <div className="container">
       <br></br>
       <br></br>
       <br></br>
@@ -13,9 +13,16 @@ export default function Home() {
       <br></br>
       <br></br>
       <br></br>
-      <div id="yellow-circle"></div>
-      <img id="home-img" src={CatImg}></img>
-      <div id="pink-box"></div>
+      <div className="row">
+        <div className="col s8">
+          <div id="yellow-circle"></div>
+          <img id="home-img" src={CatImg}></img>
+          <div id="pink-box"></div>
+        </div>
+        <div className="col s4">
+          <h2>Making my plants wet like it's my job.</h2>
+        </div>
+      </div>
     </div>
   );
 }
