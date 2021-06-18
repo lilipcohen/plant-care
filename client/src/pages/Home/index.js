088a03f0d35812b27c460a53efe8bc48a0ab599f
Lili Cohen <lili.cohen.tv@gmail.com>
2021-06-17 20:12:09 -0700

    adding circle

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -8,6 +8,12 @@ export default function Home() {
       <br></br>
       <br></br>
       <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div id="yellow-circle"></div>
       <img id="home-img" src={CatImg}></img>
       <div id="pink-box"></div>
     </div>
