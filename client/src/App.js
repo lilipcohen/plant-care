e4d97b93242510fdd9b5fa119390ae4bba57ad2c
Lili Cohen <lili.cohen.tv@gmail.com>
2021-06-17 19:36:22 -0700

    getting home page started

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Nav from './components/Nav';
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <div>
           <Nav />
 
-          {/* <Route exact path={['/', '/home']} component={Home} /> */}
+          <Route exact path={['/', '/home']} component={Home} />
           {/* <Footer /> */}
         </div>
       </Router>
---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './style.css';
+
+export default function Home() {
+  return <h2>cats are friends are friends</h2>;
+}
