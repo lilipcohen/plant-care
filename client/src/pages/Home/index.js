bfdf820f13e31523a03572144a275747aa785bd4
Lili Cohen <lili.cohen.tv@gmail.com>
2021-06-18 17:37:16 -0700

    adding photos of plants

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.css';
 import CatImg from '../../images/cat-plant-home.png';
+import PothosImg from '../../images/pothos-njoy.png';
 
 export default function Home() {
   return (
@@ -14,13 +15,34 @@ export default function Home() {
       <br></br>
       <br></br>
       <div className="row">
-        <div className="col s8">
+        <div className="col s9">
           <div id="yellow-circle"></div>
           <img id="home-img" src={CatImg}></img>
           <div id="pink-box"></div>
         </div>
-        <div className="col s4">
+        <div className="col s3">
           <h2>Making my plants wet like it's my job.</h2>
+        </div>
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div className="row">
+        <h3 className="col s12">Shop plants</h3>
+      </div>
+      <div className="row">
+        <div className="col s3">
+          <img id="home-plants" src={PothosImg}></img>
+        </div>
+        <div className="col s3">
+          <img id="home-plants" src={PothosImg}></img>
+        </div>
+        <div className="col s3">
+          <img id="home-plants" src={PothosImg}></img>
+        </div>
+        <div className="col s3">
+          <img id="home-plants" src={PothosImg}></img>
         </div>
       </div>
     </div>
