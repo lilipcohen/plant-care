ab15e7931d33f9bedc470aece3f2667ce568eb03
Lili Cohen <lili.cohen.tv@gmail.com>
2021-07-05 21:38:46 -0700

    adding text

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -104,7 +104,7 @@ export default function Home() {
             aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            culpa qui officia deserunt mollit anim id est laborum. Hellow there.
           </p>
           <button id="button-home">
             <a id="anchor" href="/plantcare">
